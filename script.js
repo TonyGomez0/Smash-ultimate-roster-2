4d7e2a65c4d930a52150b47247e77b8522031b16
TonyGomez0 <51236730+TonyGomez0@users.noreply.github.com>
2019-06-23 13:49:52 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,37 @@
+$(".1").click(function(){
+  $(".1").text("dodging towards another location (diagonal, horizontal, or vertical");
+});
+
+$(".2").click(function(){
+  $(".2").text("The involuntary movement in which the character was damaged by the oponent.");
+});
+
+$(".3").click(function(){
+   $(".3").text("The attacks the character enacts within the air.");
+});
+    
+    
+$(".4").click(function(){
+   $(".4").text("The attacks that has been protrayed from the character's representive game");
+});
+        
+        
+$(".5").click(function(){
+    $(".5").text("The ability to block enemy attacks from different location (*if used too often, then the shield durabilty would decrease causing a shield break");
+});
+    
+$(".6").click(function(){
+    $(".6").text("The ability to hold the oponent lead to a pummel or to throw them horizontally or vertically");
+});
+
+$(".7").click(function(){
+    $(".7").text("The ability to deny the launch of their character");
+});
+        
+$(".8").click(function(){
+$(".8").text("The ability to either get-up, jump, or attack from edge");
+});
+    
+$(".9").click(function(){
+    $(".9").text("The ability to increase the percentage of damage output by 1.2%");
+});
